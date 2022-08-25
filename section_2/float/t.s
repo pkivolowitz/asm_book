62b74258d015d67df7a63860e2ab5517b9e331d8
Perry Kivolowitz <perry@kivolowitz.com>
2022-08-24 20:51:14 -0500

    replaced missing file

---
+++ section_2/float/t.s
@@ -0,0 +1,16 @@
+        .text                     
+        .global main              
+        .align    2               
+    
+main:   str        x30, [sp, -16]!
+        ldr        s0, =0x3fc00000
+        fcvt       d0, s0         
+        ldr        x0, =fmt       
+        bl         printf         
+        ldr        x30, [sp], 16   
+        mov        w0, wzr         
+        ret                        
+     
+        .data                      
+fmt:    .asciz    "%f\n"           
+        .end                       
