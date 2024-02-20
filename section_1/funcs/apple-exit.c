c020675cd7287cbad80ce405ccf013dfc878885c
Perry Kivolowitz <perry@kivolowitz.com>
2024-02-20 13:59:39 -0600

    wrote apple-exit.c and s

---
+++ section_1/funcs/apple-exit.c
@@ -0,0 +1,8 @@
+/*
+*/
+
+#include <stdlib.h>
+
+int main() {
+    exit(3510);
+}
---
+++ section_1/funcs/apple-exit.s
@@ -0,0 +1,20 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 14, 0	sdk_version 14, 2
+	.globl	_main                           ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #32
+	.cfi_def_cfa_offset 32
+	stp	x29, x30, [sp, #16]             ; 16-byte Folded Spill
+	add	x29, sp, #16
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	stur	wzr, [x29, #-4]
+	mov	w0, #3510
+	bl	_exit
+	.cfi_endproc
+                                        ; -- End function
+.subsections_via_symbols
