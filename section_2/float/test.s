17f0773d7cfe3394d2be027da8a2cff604d362f7
Perry Kivolowitz <perry@kivolowitz.com>
2024-04-04 10:27:40 -0500

    added example.S and added comments to test.s

--- section_2/float/test.s
+++ section_2/float/test.s
@@ -2,12 +2,12 @@
         .p2align    2
         .global      test
 
-test:   fmov        d0, 1.0
-        fmov        d0, 1.5
-        fmov        d0, 1.75
-        fmov        d0, 1.875
-        fmov        d0, 1.9375
-        fmov        d0, 1.96875
+test:   fmov        d0, 1.0       // 
+        fmov        d0, 1.5       // 1/2
+        fmov        d0, 1.75      // 1/2 + 1/4
+        fmov        d0, 1.875     // 1/2 + 1/4 + 1/8
+        fmov        d0, 1.9375    // 1/2 + 1/4 + 1/8 + 1/16
+        fmov        d0, 1.96875   // 1/2 + 1/4 + 1/8 + 1/16 + 1/32
         ret
 
         .end
